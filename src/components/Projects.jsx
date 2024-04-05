775b6058f74440d8c9a5434ea5c42988dee14a20
Sakshi Ganesh Danej <sakshi1314.d@gmail.com>
2024-04-05 20:07:41 +0530

    project links added

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -17,32 +17,32 @@ const Projects = () => {
                             <h1>podcashh</h1>
                             <p>A free video and audio streaming platform</p>
                             <div style={{display:"flex",gap: "5px"}}>
-                            <div className="round-button"><a href="https://github.com/DanejSakshi13"><FontAwesomeIcon icon={faGithub} className="icon"/></a></div>
-                            <div className="round-button"><a href="https://github.com/DanejSakshi13"><FontAwesomeIcon icon={faArrowUpRightFromSquare} className="icon"/></a></div>
+                            <div className="round-button"><a href="https://github.com/DanejSakshi13/podcashhcopy2"><FontAwesomeIcon icon={faGithub} className="icon"/></a></div>
+                            <div className="round-button"><a href="https://github.com/DanejSakshi13/podcashhcopy2"><FontAwesomeIcon icon={faArrowUpRightFromSquare} className="icon"/></a></div>
                             </div>
                         </Carousel.Item>
                         <Carousel.Item  className="carousel-item">
                             <h1>PalettePros</h1>
                             <p>Art focused e-commerce website</p>
                             <div style={{display:"flex",gap: "5px"}}>
-                            <div className="round-button"><a href="https://github.com/DanejSakshi13"><FontAwesomeIcon icon={faGithub} className="icon"/></a></div>
-                            <div className="round-button"><a href="https://github.com/DanejSakshi13"><FontAwesomeIcon icon={faArrowUpRightFromSquare} className="icon"/></a></div>
+                            <div className="round-button"><a href="https://github.com/DanejSakshi13/Art-Ecommerce/tree/master"><FontAwesomeIcon icon={faGithub} className="icon"/></a></div>
+                            <div className="round-button"><a href="https://github.com/DanejSakshi13/Art-Ecommerce/tree/master"><FontAwesomeIcon icon={faArrowUpRightFromSquare} className="icon"/></a></div>
                             </div>
                         </Carousel.Item>
                         <Carousel.Item  className="carousel-item">
                             <h1>Medibridge</h1>
                             <p>Hospital Management System</p>
                             <div style={{display:"flex",gap: "5px"}}>
-                            <div className="round-button"><a href="https://github.com/DanejSakshi13"><FontAwesomeIcon icon={faGithub} className="icon"/></a></div>
-                            <div className="round-button"><a href="https://github.com/DanejSakshi13"><FontAwesomeIcon icon={faArrowUpRightFromSquare} className="icon"/></a></div>
+                            <div className="round-button"><a href="https://github.com/DanejSakshi13/MediBridge/tree/master"><FontAwesomeIcon icon={faGithub} className="icon"/></a></div>
+                            <div className="round-button"><a href="https://github.com/DanejSakshi13/MediBridge/tree/master"><FontAwesomeIcon icon={faArrowUpRightFromSquare} className="icon"/></a></div>
                             </div>
                         </Carousel.Item>
                         <Carousel.Item  className="carousel-item">
                             <h1>Personal Portfolio</h1>
                             <p>Personal and professional highlights</p>
                             <div style={{display:"flex",gap: "5px"}}>
-                            <div className="round-button"><a href="https://github.com/DanejSakshi13"><FontAwesomeIcon icon={faGithub} className="icon"/></a></div>
-                            <div className="round-button"><a href="https://github.com/DanejSakshi13"><FontAwesomeIcon icon={faArrowUpRightFromSquare} className="icon"/></a></div>
+                            <div className="round-button"><a href="https://github.com/DanejSakshi13/react-portfolio"><FontAwesomeIcon icon={faGithub} className="icon"/></a></div>
+                            <div className="round-button"><a href="https://react-portfolio-three-pink.vercel.app/"><FontAwesomeIcon icon={faArrowUpRightFromSquare} className="icon"/></a></div>
                             </div>
                         </Carousel.Item>
                     </Carousel>
