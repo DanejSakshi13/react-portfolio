3a5a5d2b90318c19ce7837987cc90e396073e823
Sakshi Ganesh Danej <sakshi1314.d@gmail.com>
2024-07-27 21:05:23 +0530

    added open resume feature

--- src/components/Hire.jsx
+++ src/components/Hire.jsx
@@ -1,42 +1,33 @@
 import React from "react";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import "./Hire.css";
-import { faPlay } from "@fortawesome/free-solid-svg-icons";
 import { Col, Row } from "antd";
-import dotAnimation from "../images/dot-animation.gif";
+
+
 
 export default function Hire() {
+
+
+    const handleResumeDownload = () => {
+        console.log("Download clicked"); // Debugging line
+        window.open('/Sakshi_Danej_Resume.pdf', '_blank');
+    };
+
     return (
         <div className="hire-main-section">
             <Row>
-                <Col span={12}>  <h1 className="hire-me-heading">let's get in touch</h1>
-                <div className="circle">
-                <a href="mailto:sakshi1314.d@gmail.com" className="hire-me-ripple">hire me</a>
-                </div>
+                <Col span={12}>
+                    <h1 className="hire-me-heading">let's get in touch</h1>
+                    <div className="circle">
+                        <div
+                            onClick={handleResumeDownload} // Handle click event
+                            className="hire-me-ripple"
+                        >
+                            hire me
+                        </div>
+                    </div>
                 </Col>
                 <Col span={12}> </Col>
             </Row>
         </div>
     );
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
